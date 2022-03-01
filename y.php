60670b34ce3dc44a6e5e2dbcab5b039a1088bfab
DenisHys <43570272+0xNix@users.noreply.github.com>
2022-03-01 03:39:54 -0800

    y.php

--- y.php
+++ y.php
@@ -1,12 +1,43 @@
 <html>
+ 
 <head>
-<meta content='Hacked by 0xNix' property='og:title'/>
-        <meta content='article' property='og:type'/>
-        <meta content='http://www.ratiss.org/ioport5.htm' property='og:url'/>
-        <title>Hacked by 0xNix</title>
-    <body bgcolor=black>
-    <center>
-        <body bgcolor=black><table width=100% height=100%><td align=center><span style='font: 60px fantasy;size:60px; color:#c40505; text-shadow: 0px 0px 60px #000000;'>
-        Hacked by 0xNix   </b></center>
-        <center><font color=#c6c5c4 size=3 face=courier new>.......</font></center>
-<center><table width=90% height=50%><td align=center><span style='font: 36px courier new; color:#a3a2a1;'><b>i hate myself</b></center>
+    <title>Hacked by 0xNix </title>
+    <script>alert('Explosion Squad Cyber | Wass Here!!!')</script>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+    <meta property="og:title" content="Hacked by 0xNix">
+    <meta name="description" content="wh00pz ! , your security get down">
+    <meta property="og:description" content="wh00pz ! , your security get down">
+    <meta name="theme-color" content="#fff">
+    <style type='text/css'>body,a:hover{cursor:url(../cur.cursors-4u.net/symbols/sym-1/sym46.cur),progress!important;}</style>
+</head>
+<body bgcolor="white" text="black" oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;">
+<style type="text/css">
+    center {
+        font-family: Courier;
+    }
+    img {
+        opacity: 80%;
+    }
+    red {
+        color: red;
+    }
+</style>
+<table width="100%" height="80%">
+    <td>
+        <center>
+
+               <img src="https://i.ibb.co/sJ8SL1R/oxNix.png" width="220" height="209">
+            <br>Hacked by <red><i>0xNix</i></red><br><font size="2"><br>hello i'm back xD<br></font><br>
+            <font size="2"><i>Greetz:</i><br>
+            ./KamarM4yaT404 - X404_LOSER - EXPLOSION SQUAD CYBER<br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+           
+            </font>
+    </td>
+</table>
