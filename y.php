4e44d3cb9e72a0c809befc40a67dbb51f49837ca
DenisHys <43570272+0xNix@users.noreply.github.com>
2022-03-07 08:02:18 -0800

    y.php

--- y.php
+++ y.php
@@ -1,43 +1,29 @@
-<html>
- 
+<!DOCTYPE HTML>
+<html lang="en-US">
 <head>
-    <title>Hacked by 0xNix </title>
-    <script>alert('Explosion Squad Cyber | Wass Here!!!')</script>
-    <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1" /> 
-    <meta property="og:title" content="Hacked by 0xNix">
-    <meta name="description" content="wh00pz ! , your security get down">
-    <meta property="og:description" content="wh00pz ! , your security get down">
-    <meta name="theme-color" content="#fff">
-    <style type='text/css'>body,a:hover{cursor:url(../cur.cursors-4u.net/symbols/sym-1/sym46.cur),progress!important;}</style>
+    <meta charset="UTF-8">
+    <title>Hacked by 0xNix</title>
 </head>
-<body bgcolor="white" text="black" oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;">
-<style type="text/css">
-    center {
-        font-family: Courier;
-    }
-    img {
-        opacity: 80%;
-    }
-    red {
-        color: red;
-    }
+<body background="http://4.bp.blogspot.com/-h1QH3MmXd4M/Uc64vsqmPeI/AAAAAAAAALs/Gi-fPnZ5V2Q/s1600/black+(11).jpg">
+<body>
+<script>
+    </script>
 </style>
-<table width="100%" height="80%">
-    <td>
-        <center>
-
-               <img src="https://i.ibb.co/sJ8SL1R/oxNix.png" width="220" height="209">
-            <br>Hacked by <red><i>0xNix</i></red><br><font size="2"><br>hello i'm back xD<br></font><br>
-            <font size="2"><i>Greetz:</i><br>
-            ./KamarM4yaT404 - X404_LOSER - EXPLOSION SQUAD CYBER<br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-           
-            </font>
-    </td>
-</table>
+ 
+<center><br><img src="https://image.ibb.co/hWAnfA/IMG-20181110-015020.jpg" width="400" height="350"><center>
+<body>
+ 
+<font face= "Architects Daughter">
+<font color='red' face='Architects Daughter' font size="5px"'>Hacked by 0xNix | ESC Team</font></b><center>
+<?php
+echo '<big><font color=white> System: <span style="color: lime;">'.php_uname().'</span></big><br>';
+echo "<font color=white>Your IP: <font color=blue>".$_SERVER['REMOTE_ADDR']."</font><br>";
+?>
+</body>
+<br>
+<br>
+<br>
+<style> .error { text-align: center; font-family: 'Gilda Display', serif; -webkit-animation: noise-3 1s linear infinite; animation: noise-3 1s linear infinite; overflow: default; } .info { text-align: center; width: 200px; height: 60px; margin: auto; position: absolute; top: 280px; bottom: 0; left: 20px; right: 0; -webkit-animation: noise-3 1s linear infinite; animation: noise-3 1s linear infinite; } .info:after { content: 'OWNED'; font-family: OCR-A; font-size: 100px; text-align: center; width: 800px; margin: auto; position: absolute; top: 20px; bottom: 0; left: 40px; right: 0; opacity: 0; color: white; -webkit-animation: noise-1 .2s linear infinite; animation: noise-1 .2s linear infinite; } @-webkit-keyframes noise-1 { 0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;} 10% {opacity: .1;} 50% {opacity: .5; left: -6px;} 80% {opacity: .3;} 100% {opacity: .6; left: 2px;} } @keyframes noise-1 { 0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;} 10% {opacity: .1;} 50% {opacity: .5; left: -6px;} 80% {opacity: .3;} 100% {opacity: .6; left: 2px;} } @-webkit-keyframes noise-2 { 0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;} 10% {opacity: .1;} 50% {opacity: .5; left: 6px;} 80% {opacity: .3;} 100% {opacity: .6; left: -2px;} } @keyframes noise-2 { 0%, 20%, 40%, 60%, 70%, 90% {opacity: 0;} 10% {opacity: .1;} 50% {opacity: .5; left: 6px;} 80% {opacity: .3;} 100% {opacity: .6; left: -2px;} } @-webkit-keyframes noise { 0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);} 4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);} 43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);} } @keyframes noise { 0%, 3%, 5%, 42%, 44%, 100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);} 4.3% {opacity: 1; -webkit-transform: scaleY(1.7); transform: scaleY(1.7);} 43% {opacity: 1; -webkit-transform: scaleX(1.5); transform: scaleX(1.5);} } @-webkit-keyframes noise-3 { 0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);} 4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);} 43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate (60deg);} } @keyframes noise-3 { 0%,3%,5%,42%,44%,100% {opacity: 1; -webkit-transform: scaleY(1); transform: scaleY(1);} 4.3% {opacity: 1; -webkit-transform: scaleY(4); transform: scaleY(4);} 43% {opacity: 1; -webkit-transform: scaleX(10) rotate(60deg); transform: scaleX(10) rotate (60deg);} } .wrap { top: 30%; left: 25%; height: 200px; margin-top: -100px; position: absolute; } code { color: white; } span.blue { color: #48beef; } span.comment { color: #7f8c8d; } span.orange { color: #f39c12; } span.green { color: #33cc33; } .viewFull { font-family:OCR-A; color:orange; text-decoration:; }    } @media only screen and (min-height: 500px) { .viewFull{ display:none;        } }     </style> <center> <style> img { filter:alpha(opacity=70); -moz-opacity:0.7; opacity:0.7; } img:hover { filter:alpha(opacity=100); -moz-opacity:.0; opacity:1.0; } </style>
+<a href='https://www.facebook.com/EscTeam2018/' target='_blank'><font size='3px'>- About Us -</a>
+</body>
+</html>
